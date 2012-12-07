d7102f1d0acdff25750c0deb70592f4214bb9494
duynguyen <nguyentuongduy1988@gmail.com>
2012-12-07 22:07:56 +0700

    clear

---
+++ includes/ticket.api.php
@@ -0,0 +1,152 @@
+
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+* Act on profiles being loaded from the database.
+*
+* This hook is invoked during profile loading, which is handled by
+* entity_load(), via the EntityCRUDController.
+*
+* @param $entities
+*   An array of ticket entities being loaded, keyed by id.
+*
+* @see hook_entity_load()
+*/
+function hook_ticket_load($entities) {
+  $result = db_query('SELECT ticket_id, foo FROM {mytable} WHERE ticket_id IN(:ids)', array(':ids' => array_keys($entities)));
+  foreach ($result as $record) {
+    $entities[$record->ticket_id]->foo = $record->foo;
+  }
+}
+
+/**
+* Respond when a profile is inserted.
+*
+* This hook is invoked after the profile is inserted into the database.
+*
+* @param profile
+*   The profile that is being inserted.
+*
+* @see hook_entity_insert()
+*/
+function hook_ticket_insert($ticket) {
+  db_insert('mytable')
+    ->fields(array(
+      'ticket_id' => $ticket->ticket_id,
+      'extra' => $ticket->extra,
+    ))
+    ->execute();
+}
+
+/**
+* Act on a profile being inserted or updated.
+*
+* This hook is invoked before the profile is saved to the database.
+*
+* @param $ticket
+*   The profile that is being inserted or updated.
+*
+* @see hook_entity_presave()
+*/
+function hook_ticket_presave($ticket) {
+  $ticket->extra = 'foo';
+}
+
+/**
+* Respond to a profile being updated.
+*
+* This hook is invoked after the profile has been updated in the database.
+*
+* @param $ticket
+*   The $ticket that is being updated.
+*
+* @see hook_entity_update()
+*/
+function hook_ticket_update($ticket) {
+  db_update('mytable')
+    ->fields(array('extra' => $ticket->extra))
+    ->condition('ticket_id', $ticket->ticket_id)
+    ->execute();
+}
+
+/**
+* Respond to profile deletion.
+*
+* This hook is invoked after the profile has been removed from the database.
+*
+* @param $ticket
+*   The profile that is being deleted.
+*
+* @see hook_entity_delete()
+*/
+function hook_ticket_delete($ticket) {
+  db_delete('mytable')
+    ->condition('ticket_id', $ticket->ticket_id)
+    ->execute();
+}
+
+/**
+* Act on a profile that is being assembled before rendering.
+*
+* @param $ticket
+*   The profile entity.
+* @param $view_mode
+*   The view mode the profile is rendered in.
+* @param $langcode
+*   The language code used for rendering.
+*
+* The module may add elements to $ticket->content prior to rendering. The
+* structure of $ticket->content is a renderable array as expected by
+* drupal_render().
+*
+* @see hook_entity_prepare_view()
+* @see hook_entity_view()
+*/
+function hook_ticket_view($ticket, $view_mode, $langcode) {
+  $ticket->content['my_additional_field'] = array(
+    '#markup' => $additional_field,
+    '#weight' => 10,
+    '#theme' => 'mymodule_my_additional_field',
+  );
+}
+
+/**
+* Alter the results of entity_view() for profiles.
+*
+* @param $build
+*   A renderable array representing the profile content.
+*
+* This hook is called after the content has been assembled in a structured
+* array and may be used for doing processing which requires that the complete
+* profile content structure has been built.
+*
+* If the module wishes to act on the rendered HTML of the profile rather than
+* the structured content array, it may use this hook to add a #post_render
+* callback. Alternatively, it could also implement hook_preprocess_ticket().
+* See drupal_render() and theme() documentation respectively for details.
+*
+* @see hook_entity_view_alter()
+*/
+function hook_ticket_view_alter($build) {
+  if ($build['#view_mode'] == 'full' && isset($build['an_additional_field'])) {
+    // Change its weight.
+    $build['an_additional_field']['#weight'] = -10;
+
+    // Add a #post_render callback to act on the rendered HTML of the entity.
+    $build['#post_render'][] = 'my_module_post_render';
+  }
+}
+
+
--- lib/TicketEntityMetaController.php
+++ lib/TicketEntityMetaController.php
@@ -1,16 +1,57 @@
 <?php
-
+/**
+ * View can see this properties
+ */
 class TicketEntityMetaController extends EntityDefaultMetadataController {
   public function entityPropertyInfo() {
-    $info['properties'] = array(
-      'uid'     => array('type' => 'user',  'label' => 'Author'),
-      //'term'    => array('type' => 'string',  'label' => 'Term'),
-      //'review'  => array('type' => 'integer', 'label' => 'Review'),
-      //'group'   => array('type' => 'integer', 'label' => 'Group'),
-      'created' => array('type' => 'date',    'label' => 'Created'),
-      'changed' => array('type' => 'date',    'label' => 'Change'),
+    $info = parent::entityPropertyInfo();
+    $properties = &$info[$this->type]['properties'];
+
+    $properties['title'] = array(
+      'label' => t('Title'),
+      'description' => t('The ticket title.'),
+      'setter callback' => 'entity_property_verbatim_set',
+      'setter permission' => 'administer ticket',
+      'schema field' => 'title',
     );
 
-    return array($this->type => $info);
+    $properties['type'] = array(
+      'type' => 'ticket',
+      'getter callback' => 'entity_property_getter_method',
+      'setter callback' => 'entity_property_verbatim_set',
+      'setter permission' => 'administer ticket',
+      'required' => TRUE,
+      'description' => t('The ticket type.'),
+    ) + $properties['type'];
+
+    unset($properties['uid']);
+
+    $properties['user'] = array(
+      'label' => t("User"),
+      'type' => 'user',
+      'description' => t("The owner of the ticket."),
+      'getter callback' => 'entity_property_getter_method',
+      'setter callback' => 'entity_property_setter_method',
+      'setter permission' => 'administer ticket',
+      'required' => TRUE,
+      'schema field' => 'uid',
+    );
+
+    $properties['created'] = array(
+      'label' => t("Date created"),
+      'type' => 'date',
+      'description' => t("The date the ticket was created."),
+      'setter callback' => 'entity_property_verbatim_set',
+      'setter permission' => 'administer ticket',
+      'schema field' => 'created',
+    );
+    $properties['changed'] = array(
+      'label' => t("Date changed"),
+      'type' => 'date',
+      'schema field' => 'changed',
+      'description' => t("The date the ticket was most recently updated."),
+    );
+
+    return $info;
   }
 }
